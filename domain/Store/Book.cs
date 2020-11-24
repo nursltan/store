aabd33ec85619b85bc2d95ac001966668252fd06
nursultanmuratjanov1@gmail.com <nursultanmuratjanov1@gmail.com>
2020-11-24 17:51:30 +0600

    Описать сущность Book

---
+++ domain/Store/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Store
+{
+    public class Book //Класс для хранения книг
+    {
+        public int Id { get; }
+        public string Title { get; } //только для чтения
+
+        public Book(int id,string title) //конструктор
+        {
+            Id = id;
+            Title = title;
+        }
+    }
+}
