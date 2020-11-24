f3a0f62defe76e7fd591607b752eb33421016ede
nursultanmuratjanov1@gmail.com <nursultanmuratjanov1@gmail.com>
2020-11-24 18:32:51 +0600

    Добавить реализацию репозиория книг в памяти

---
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
