8a8a25620384b5245324b4c48f5cc40d281c1451
nursultanmuratjanov1@gmail.com <nursultanmuratjanov1@gmail.com>
2020-11-25 18:47:45 +0600

    Реализовать поиск по ISBN и по автору

--- domain/Store/Book.cs
+++ domain/Store/Book.cs
@@ -1,16 +1,34 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Store
 {
     public class Book //Класс для хранения книг
     {
         public int Id { get; }
+
+        public string Isbn { get; }
+
+        public string Author { get; }
         public string Title { get; } //только для чтения
 
-        public Book(int id,string title) //конструктор
+        public Book(int id,string isbn,string author, string title) //конструктор
         {
             Id = id;
+            Isbn = isbn;
+            Author = author;
             Title = title;
+        }
+
+        internal static bool IsIsbn(string s)
+        {
+            if (s == null)
+                return false;
+            s = s.Replace("-", "") // Дефисы и пробелы заменяем 
+                .Replace(" ", "") // на пустые строки
+                .ToUpper(); // приводим к верхнему регистру
+
+            return Regex.IsMatch(s, @"^ISBN\d{10}(\d{3})?$");
         }
     }
 }
--- infrastructure/Store.Memory/BookRepository.cs
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -7,15 +7,21 @@ namespace Store.Memory
     {
         private readonly Book[] books = new[]
         {
-            new Book(1, "Art of Programming"),
-            new Book(2, "Refractoring"),
-            new Book(3, "C Programming Language"),
+            new Book(1,"ISBN 12312-31231","D. Knuth", "Art of Programming"),
+            new Book(2,"ISBN 12312-31232","M. Fowler", "Refractoring"),
+            new Book(3, "ISBN 12312-31233","B. Kernighan, D. Ritchie","C Programming Language"),
         };
 
-
-        public Book[] GetAllByTitle(string titlePart)
+        public Book[] GetAllByIsbn(string isbn)
         {
-            return books.Where(book => book.Title.Contains(titlePart))
+            return books.Where(book => book.Isbn == isbn)
+                .ToArray();
+        }
+
+        public Book[] GetAllByTitleOrAuthor(string query)
+        {
+            return books.Where(book => book.Author.Contains(query)
+                                    ||  book.Title.Contains(query))
                 .ToArray();
         }
     }
