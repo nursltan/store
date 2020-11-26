4be9036780247fce4b9df82d0c30d529f3624836
nursultanmuratjanov1@gmail.com <nursultanmuratjanov1@gmail.com>
2020-11-26 17:37:48 +0600

    Добавить страницу описание книги

--- infrastructure/Store.Memory/BookRepository.cs
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -7,9 +7,15 @@ namespace Store.Memory
     {
         private readonly Book[] books = new[]
         {
-            new Book(1,"ISBN 12312-31231","D. Knuth", "Art of Programming"),
-            new Book(2,"ISBN 12312-31232","M. Fowler", "Refractoring"),
-            new Book(3, "ISBN 12312-31233","B. Kernighan, D. Ritchie","C Programming Language"),
+            new Book(1,"ISBN 0321751043","D. Knuth", "Art of Programming",
+                "Finally, after a wait of more than thirty-five years, the first part of Volume 4 is at last ready for publication.",
+                7.19m),
+            new Book(2,"ISBN 0201485672","M. Fowler", "Refactoring",
+                "Whenever you read [Refactoring], it’s time to read it again. And if you haven’t read it yet, please do before writing another line of code.",
+                12.45m),
+            new Book(3, "ISBN 0131101633","B. Kernighan, D. Ritchie","C Programming Language",
+                "Known as the bible of C, this classic bestseller introduces the C programming language and illustrates algorithms, data structures, and programming techniques.", 
+                14.98m),
         };
 
         public Book[] GetAllByIsbn(string isbn)
@@ -23,6 +29,11 @@ namespace Store.Memory
             return books.Where(book => book.Author.Contains(query)
                                     ||  book.Title.Contains(query))
                 .ToArray();
+        }
+
+        public Book GetById(int id)
+        {
+            return books.Single(book => book.Id == id);
         }
     }
 }
